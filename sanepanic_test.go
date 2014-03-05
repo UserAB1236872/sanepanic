034e45a326ddc9f739b9f7cbebf82519b7455e89
Jsor <Jragonmiris@gmail.com>
2014-03-04 22:26:00 -0700

    Added tests

---
+++ sanepanic_test.go
@@ -0,0 +1,124 @@
+package sanepanic_test
+
+import (
+	"github.com/Jragonmiris/sanepanic"
+	"sync"
+	"testing"
+)
+
+func TestSomething(t *testing.T) {
+	defer func() {
+		err := recover()
+		if err == nil {
+			t.Fatal("No panic")
+		}
+	}()
+
+	panic("Panic!")
+}
+
+func TestCatchAll(t *testing.T) {
+	mu := &sync.Mutex{}
+	i := 0
+	wg := &sync.WaitGroup{}
+
+	handler := func(info sanepanic.Info) bool {
+		blankInfo := sanepanic.Info{}
+		if info == blankInfo {
+			t.Errorf("No panic info exists")
+		} else {
+			t.Logf("Received valid panic data: %v", info)
+		}
+
+		mu.Lock()
+		defer mu.Unlock()
+		i++
+
+		return true
+	}
+
+	sanepanic.SetHandlerFunc(handler)
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		go func() {
+			defer wg.Done()
+			defer sanepanic.Forward()
+			panic("Arghlbarg")
+		}()
+	}
+
+	wg.Wait()
+}
+
+func TestCatchSome(t *testing.T) {
+	mu := &sync.Mutex{}
+	i := 0
+	wg := &sync.WaitGroup{}
+
+	handler := func(info sanepanic.Info) bool {
+		blankInfo := sanepanic.Info{}
+		if info == blankInfo {
+			t.Errorf("No panic info exists")
+		} else {
+			t.Logf("Received valid panic data: %v", info)
+		}
+
+		mu.Lock()
+		defer mu.Unlock()
+		i++
+		if i == 5 {
+			return false
+		} else if i > 5 {
+			t.Fatalf("Forwarded too many panics")
+		}
+
+		return true
+	}
+
+	sanepanic.SetHandlerFunc(handler)
+
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			defer sanepanic.Forward()
+			panic("Arghlbarg")
+		}()
+	}
+
+	wg.Wait()
+}
+
+func TestNested(t *testing.T) {
+	genHandler := func(quit chan struct{}) sanepanic.HandlerFunc {
+		return func(sanepanic.Info) bool {
+			close(quit)
+			return false
+		}
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		quit := make(chan struct{})
+		ph := sanepanic.NewHandler(genHandler(quit))
+		for i := 0; i < 10; i++ {
+			i := i
+			wg.Add(1)
+
+			go func() {
+				defer wg.Done()
+				defer ph.Forward()
+				if i == 9 {
+					panic(i)
+				}
+
+				<-quit
+			}()
+		}
+
+		wg.Done()
+	}()
+
+	wg.Wait() // Will deadlock if test fails
+}
