a6385ce66db5f858029e51fce44894138df13d3c
Jsor <Jragonmiris@gmail.com>
2014-03-04 22:18:23 -0700

    Initial commit

--- handler.go
+++ handler.go
@@ -5,6 +5,9 @@
 //
 // Similar to the log package, this provides a PanicHandler type which allows for multiple "instances" of this package to be run at once,
 // if for whatever reason you wish to split up a single process into multiple chunks that handle their panicking individually.
+//
+// One note about the handler is that if the handler ceases (as with calling Done or returning "false" in your HandlerFunc)
+// any panics in functions running the handler will pass silently without being acknowledged.
 package sanepanic
 
 import (
@@ -19,39 +22,41 @@ import (
 //
 // StackTrace is the information returned by runtime.Stack at the time Handler is called. Due to the way panic and defer
 // work in Go, this stack trace will print the line your code panicked on.
-type PanicInfo struct {
+type Info struct {
 	Info       interface{}
 	StackTrace string
 }
 
+// A HandlerFunc handles a panic and returns true if the panic
+// handler should continue running
+type HandlerFunc func(Info) bool
+
 var (
-	internalPanicHandler *PanicHandler
+	internalPanicHandler *Handler
 	mu                   *sync.Mutex
 )
 
 // Automatically called when the package is imported (but only called once per program execution)
 func init() {
-	internalPanicHandler = NewPanicHandler(DefaultCleanupHandler)
+	internalPanicHandler = NewHandler(DefaultHandlerFunc)
 	mu = &sync.Mutex{}
 }
 
-// Restart should be called if a panic is recovered from successfully and the program
-// is deemed worthy to continue.
-//
-// Strictly speaking, it can be called whenever, but it will seem to an outside observer to be a no-op
+// Restart should be called if the handler is inadvertantly cancelled.
+// It automatically registers the same HandlerFunc the previous Handler was using
 func Restart() {
 	mu.Lock()
 	defer mu.Unlock()
 	internalPanicHandler.Done()
-	internalPanicHandler = NewPanicHandler(internalPanicHandler.cleanupFunc)
+	internalPanicHandler = NewHandler(internalPanicHandler.handle)
 }
 
 // Allows you to tailor your recovery function to the PanicInfo forwarded to the listener
 // you should almost always set this, since the default handler is basically just panic() without the program termination
-func SetCleanupHandler(newCleanupFunc func(PanicInfo)) {
+func SetHandlerFunc(newHandler HandlerFunc) {
 	mu.Lock()
 	defer mu.Unlock()
-	internalPanicHandler.SetCleanupHandler(newCleanupFunc)
+	internalPanicHandler.SetHandlerFunc(newHandler)
 }
 
 // Exits the listener if no panics have been received, or waits until panic handling has been done
@@ -64,15 +69,17 @@ func Done() {
 
 // At the beginning of any Goroutine, call "defer sanepanic.Handler()"
 // to forward the panic to the package's listener and call your cleanup handling function
-func Handle() {
+func Forward() {
 	mu.Lock()
 	defer mu.Unlock()
-	internalPanicHandler.Handle()
+	err := recover() // Have to do recover directly in deferred function
+	internalPanicHandler.forward(err)
 }
 
 // Prints a panic almost exactly like the runtime does, except the program doesn't exit.
-func DefaultCleanupHandler(info PanicInfo) {
+func DefaultHandlerFunc(info Info) bool {
 	fmt.Fprintf(os.Stderr, "Panic: %v\n%s", info.Info, info.StackTrace)
+	return true
 }
 
 /* Actual implementation, to use if you want multiple central handlers for some reason */
@@ -80,72 +87,75 @@ func DefaultCleanupHandler(info PanicInfo) {
 // A central processor for panicking. This more or less duplicates the functionality of the package.
 // The only missing function is Restart() which can be emulated by calling YourPanicHandler.Done() followed by creating
 // a new one.
-type PanicHandler struct {
-	panicChan   chan PanicInfo
-	wg          *sync.WaitGroup
-	cleanupFunc func(PanicInfo)
-	mu          *sync.Mutex
+type Handler struct {
+	panicChan chan Info
+	quit      chan struct{}
+	handle    HandlerFunc
+	mu        *sync.Mutex
 }
 
 // Creates a new panic handler AND makes it start listening for panics.
-func NewPanicHandler(cleanupFunc func(PanicInfo)) *PanicHandler {
-	ph := &PanicHandler{panicChan: make(chan PanicInfo), wg: &sync.WaitGroup{}, cleanupFunc: cleanupFunc, mu: &sync.Mutex{}}
+func NewHandler(handler HandlerFunc) *Handler {
+	ph := &Handler{panicChan: make(chan Info), handle: handler, mu: &sync.Mutex{}, quit: make(chan struct{})}
 	go ph.listen()
 	return ph
 }
 
-// Handles exactly one panic
-func (ph *PanicHandler) listen() {
-	ph.wg.Add(1)
-	info, ok := <-ph.panicChan
-	if ok {
-		ph.handlepanic(info)
-	} else {
-		close(ph.panicChan)
-		ph.wg.Done()
+// Handles panics
+func (ph *Handler) listen() {
+	for info := range ph.panicChan {
+		if !ph.handleForwardedPanic(info) {
+			close(ph.quit)
+			break
+		}
 	}
 }
 
-func (ph *PanicHandler) handlepanic(info PanicInfo) {
+func (ph *Handler) handleForwardedPanic(info Info) bool {
 	ph.mu.Lock()
-	ph.cleanupFunc(info)
-	close(ph.panicChan)
-	ph.wg.Done()
-	mu.Unlock()
+	defer ph.mu.Unlock()
+	return ph.handle(info)
 }
 
 // Stops the listener (if it has not already been used). If a panic has been detected, waits for the processing to be done
 // before proceeding
-func (ph *PanicHandler) Done() {
+func (ph *Handler) Done() {
 	select {
 	case info, ok := <-ph.panicChan: // Handles the case where we somehow do this exactly when a panic is sent
 		if ok {
-			ph.handlepanic(info)
+			close(ph.quit)
+			close(ph.panicChan)
+			ph.mu.Lock()
+			defer ph.mu.Unlock()
+			ph.handleForwardedPanic(info)
 		}
 	default: // Only executes if no panics were sent AND panicChan has yet to be closed
 		close(ph.panicChan)
 	}
-	ph.wg.Wait()
 }
 
 // Swaps out the panic handling functions provided at construction.
-func (ph *PanicHandler) SetCleanupHandler(newCleanupFunc func(PanicInfo)) {
+func (ph *Handler) SetHandlerFunc(newHandler HandlerFunc) {
 	ph.mu.Lock()
 	defer ph.mu.Unlock()
-	ph.cleanupFunc = newCleanupFunc
+	ph.handle = newHandler
 }
 
-// As with the package level function, calling defer YourPanicHandler.Handle()
+// As with the package level function, calling defer YourPanicHandler.Forward()
 // at the top of a panicky goroutine will allow it to be processed by this panic handler.
-func (ph *PanicHandler) Handle() {
+func (ph *Handler) Forward() {
 	err := recover()
+	ph.forward(err)
+}
+
+func (ph *Handler) forward(err interface{}) {
 	if err != nil {
 		buf := make([]byte, 10000)
 		traceSize := runtime.Stack(buf, true)
 		buf = buf[:traceSize]
 		select {
-		case ph.panicChan <- PanicInfo{Info: err, StackTrace: string(buf)}:
-		default:
+		case ph.panicChan <- Info{Info: err, StackTrace: string(buf)}:
+		case <-ph.quit:
 		}
 	}
 }
